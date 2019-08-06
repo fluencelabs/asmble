728b78d7135b50e24f066ca1950ccd48537d8eec
vms <michail.vms@gmail.com>
2019-08-06 17:25:48 +0300

    add EIC metering (#12)

--- compiler/src/main/kotlin/asmble/run/jvm/EnvModule.kt
+++ compiler/src/main/kotlin/asmble/run/jvm/EnvModule.kt
@@ -1,11 +1,14 @@
 package asmble.run.jvm
 
 /**
- * Module used for metering.
+ * Module used for gas and EIC metering.
  */
-open class EnvModule(val gasLimit: Long) {
+open class EnvModule(private val gasLimit: Long) {
 
     private var overallSpentGas: Long = 0
+
+    // executed instruction counter
+    private var EIC: Long = 0
 
     /**
      * [Wasm function]
@@ -20,11 +23,20 @@ open class EnvModule(val gasLimit: Long) {
     }
 
     /**
-     * Sets spent gas value. Used from WasmVm to clear gas value before metering.
-     * It should be impossible to call this function from a wasm module.
+     * [Wasm function]
+     * Adds EIC to overall executed instruction counter.
      */
-    fun setSpentGas(value: Long) {
-        overallSpentGas = value;
+    fun eic(currentEIC: Int) {
+        EIC += currentEIC;
+    }
+
+    /**
+     * Sets spent gas and EIC value to 0. Used from WasmVm to clear gas value before metering.
+     * It should be impossible to call this function from a Wasm module.
+     */
+    fun clearState() {
+        overallSpentGas = 0;
+        EIC = 0;
     }
 
     /**
@@ -34,4 +46,10 @@ open class EnvModule(val gasLimit: Long) {
         return overallSpentGas;
     }
 
+    /**
+     * Returns current EIC. Used from WasmVm to determine count of executed instruction after each invocation.
+     */
+    fun getEIC(): Long {
+        return EIC;
+    }
 }
