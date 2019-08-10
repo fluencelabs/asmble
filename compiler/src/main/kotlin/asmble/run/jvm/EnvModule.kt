4c65740d03b7a0723d0d4286bf2fa46c461171a0
vms <michail.vms@gmail.com>
2019-08-10 14:52:25 +0300

    initial commit (#13)

--- compiler/src/main/kotlin/asmble/run/jvm/EnvModule.kt
+++ compiler/src/main/kotlin/asmble/run/jvm/EnvModule.kt
@@ -1,33 +1,39 @@
 package asmble.run.jvm
 
 /**
+ * Used to tack the state of the environment module.
+ */
+data class EnvState(
+        var spentGas: Long = 0,
+        // executed instruction counter
+        var EIC: Long = 0
+)
+
+/**
  * Module used for gas and EIC metering.
  */
 open class EnvModule(private val gasLimit: Long) {
 
-    private var overallSpentGas: Long = 0
-
-    // executed instruction counter
-    private var EIC: Long = 0
+    private var state = EnvState();
 
     /**
      * [Wasm function]
      * Adds spent gas to overall spent gas and checks limit exceeding.
      */
     fun gas(spentGas: Int) {
-        if(overallSpentGas + spentGas > gasLimit) {
+        if(state.spentGas + spentGas > gasLimit) {
             // TODO : check for overflow, throw an exception
         }
 
-        overallSpentGas += spentGas;
+        state.spentGas += spentGas;
     }
 
     /**
      * [Wasm function]
      * Adds EIC to overall executed instruction counter.
      */
-    fun eic(currentEIC: Int) {
-        EIC += currentEIC;
+    fun eic(EIC: Int) {
+        state.EIC += EIC;
     }
 
     /**
@@ -35,21 +41,16 @@ open class EnvModule(private val gasLimit: Long) {
      * It should be impossible to call this function from a Wasm module.
      */
     fun clearState() {
-        overallSpentGas = 0;
-        EIC = 0;
+        state.spentGas = 0;
+        state.EIC = 0;
     }
 
     /**
-     * Returns spent gas. Used from WasmVm to determine spent gas after each invocation.
+     * Returns environment module state.
+     * Used from WasmVm to determine spent gas and executed instruction counter after each invocation.
      */
-    fun getSpentGas(): Long {
-        return overallSpentGas;
+    fun getState(): EnvState {
+        return state;
     }
 
-    /**
-     * Returns current EIC. Used from WasmVm to determine count of executed instruction after each invocation.
-     */
-    fun getEIC(): Long {
-        return EIC;
-    }
 }
